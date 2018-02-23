a49918822d67afcfd0691cfba8cf7f5d1d1f19fa
Abhishek Sarkar <aksarkar@alum.mit.edu>
2018-02-23 13:18:26 -0600

    Back the browser with the database

--- browser/main.py
+++ browser/main.py
@@ -5,56 +5,40 @@ import bokeh.io
 import bokeh.layouts
 import bokeh.models
 import bokeh.plotting
+import os.path
 import numpy as np
 import pandas as pd
+import sqlite3
 
-gene_info = (pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/scqtl-genes.txt.gz')
-             .set_index('gene')
-             .query('source == "H. sapiens"')
-             .query('chr != "hsX"')
-             .query('chr != "hsY"')
-             .query('chr != "hsMT"'))
-means = pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/zi2-mean.txt.gz', index_col='gene', sep=' ')
-genotypes = pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/bulk-qtl-genotypes.txt.gz', index_col='gene', sep=' ')
-gene = genotypes.iloc[0].name
+def init(ind_data):
+  with sqlite3.connect(os.path.join(os.path.dirname(__file__), 'browser.db')) as conn:
+    gene = next(conn.execute('select gene from qtls order by p_beta limit 1;'))[0]
+    ind_data.data = bokeh.models.ColumnDataSource.from_df(pd.read_sql(
+      sql="""select genotype.value as genotype, log_mean.value as mean, bulk.value as bulk 
+         from log_mean, bulk, genotype 
+         where log_mean.gene == bulk.gene and log_mean.ind == bulk.ind 
+         and genotype.gene == bulk.gene and genotype.ind == bulk.ind and
+         log_mean.gene == ?""",
+      params=(gene,),
+      con=conn))
 
-g, m = genotypes.loc[gene].align(means.loc[gene], join='inner')
-sc_mean_by_geno = bokeh.plotting.figure(title=gene_info.loc[gene]['name'], width=500, height=400, tools=[])
-sc_mean_by_geno.scatter(x=g.values, y=m.values, size=8)
+# These need to be separate because they have different dimension
+ind_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['genotype', 'mean', 'bulk']))
+init(ind_data)
+
+sc_mean_by_geno = bokeh.plotting.figure(width=600, height=400, tools=['tap'])
+sc_mean_by_geno.scatter(source=ind_data, x='genotype', y='mean', size=8)
 sc_mean_by_geno.xaxis.axis_label = 'Centered genotype'
-sc_mean_by_geno.yaxis.axis_label = 'Estimated single cell mean expression'
+sc_mean_by_geno.yaxis.axis_label = 'Estimated single cell log mean expression'
 
-annotations = pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/scqtl-annotation.txt')
-keep_samples = pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/quality-single-cells.txt', index_col=0, header=None)
-annotations = annotations.loc[keep_samples.values.ravel()]
-
-keep_genes = pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/genes-pass-filter.txt', index_col=0, header=None)
-umi = np.array([])
-for chunk in pd.read_table('/home/aksarkar/projects/singlecell-qtl/data/scqtl-counts.txt.gz', index_col=0, chunksize=100):
-  if gene in chunk.index:
-    umi = chunk.loc[gene,keep_samples.values.ravel()]
-    break
-
-umi_by_ind = bokeh.plotting.figure(title=gene_info.loc[gene]['name'], plot_width=1000, plot_height=450, tools=[])
-max_ = 20
-hist, edges = np.histogram(umi[umi < max_], bins=np.arange(max_ + 1))
-umi_by_ind.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], color='black')
-umi_by_ind.xaxis.axis_label = 'Observed UMI count'
-umi_by_ind.yaxis.axis_label = 'Number of cells'
-
-bulk = (pd.read_table('/project2/gilad/singlecell-qtl/bulk/counts_RNAseq_iPSC.txt', sep=' ', index_col='gene')
-        .rename(index=lambda x: x.split('.')[0], columns=lambda x: 'NA{}'.format(x))
-        .align(means, axis=None)[0])
-bulk /= bulk.sum(axis=0)
-
-b, _ = bulk.loc[gene].align(means.loc[gene], join='inner')
-bulk_mean_by_geno = bokeh.plotting.figure(title=gene_info.loc[gene]['name'], width=500, height=400, tools=[])
-bulk_mean_by_geno.scatter(x=g.values, y=b.values, size=8)
+bulk_mean_by_geno = bokeh.plotting.figure(width=600, height=400, tools=['tap'])
+bulk_mean_by_geno.scatter(source=ind_data, x='genotype', y='bulk', size=8)
 bulk_mean_by_geno.xaxis.axis_label = 'Centered genotype'
-bulk_mean_by_geno.yaxis.axis_label = 'Bulk CPM'
+bulk_mean_by_geno.yaxis.axis_label = 'Bulk log CPM'
 
-layout = bokeh.layouts.layout([[sc_mean_by_geno, bulk_mean_by_geno], [umi_by_ind]])
+layout = bokeh.layouts.layout([[sc_mean_by_geno, bulk_mean_by_geno]])
 
 doc = bokeh.io.curdoc()
 doc.title = 'scQTL browser'
 doc.add_root(layout)
+
