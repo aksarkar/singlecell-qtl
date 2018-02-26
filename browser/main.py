ab8b8832c6dd917594edee611aaa07f85b51f54f
Abhishek Sarkar <aksarkar@alum.mit.edu>
2018-02-26 16:15:39 -0600

    Fix UMI panel

--- browser/main.py
+++ browser/main.py
@@ -12,17 +12,28 @@ import scipy.stats as st
 import scipy.special as sp
 import sqlite3
 
+# This needs to be global to be visible to callbacks
+gene = None
+
 def update_gene(attr, old, new):
   selected = gene_data.selected['1d']['indices']
   if not selected:
     return
   with sqlite3.connect(os.path.join(os.path.dirname(__file__), 'browser.db')) as conn:
+    global gene
     gene = next(conn.execute('select gene from qtls where qtls.gene == ?;', (gene_data.data['gene'][selected[0]],)))[0]
+    print('Selected {}'.format(gene))
     ind_data.data = bokeh.models.ColumnDataSource.from_df(pd.read_sql(
-      sql="""select genotype.ind, genotype.value as genotype, log_mean.value as mean, bulk.value as bulk 
-          from log_mean, bulk, genotype 
-          where log_mean.gene == bulk.gene and log_mean.ind == bulk.ind 
-          and genotype.gene == bulk.gene and genotype.ind == bulk.ind and
+      sql="""select genotype.ind, genotype.value as genotype, 
+          log_mean.value as mean, log_disp.value as disp, 
+          logodds.value as logodds, bulk.value as bulk 
+
+          from log_mean, log_disp, logodds, bulk, genotype 
+
+          where log_mean.gene == log_disp.gene and log_mean.ind == log_disp.ind and
+          log_disp.gene == logodds.gene and log_disp.ind == logodds.ind and
+          logodds.gene == bulk.gene and logodds.ind == bulk.ind and 
+          genotype.gene == bulk.gene and genotype.ind == bulk.ind and
           log_mean.gene == ?""",
       params=(gene,),
       con=conn))
@@ -31,8 +42,8 @@ def update_umi(attr, old, new):
   selected = ind_data.selected['1d']['indices']
   with sqlite3.connect(os.path.join(os.path.dirname(__file__), 'browser.db')) as conn:
     if selected:
-      gene = next(conn.execute('select gene from qtls where qtls.gene == ?;', (gene_data.data['gene'][selected[0]],)))[0]
       ind = ind_data.data['ind'][selected[0]]
+      print("Selected {}, {}".format(ind, gene))
       umi = pd.read_sql(
         """select umi.value, annotation.size from annotation, umi 
         where umi.gene == ? and annotation.chip_id == ? and 
@@ -57,7 +68,6 @@ def update_umi(attr, old, new):
       exp_count = umi.shape[0] * np.array([G(x).mean() for x in grid])
       exp_count[0] *= sp.expit(logodds)
       exp_count[0] += (umi['value'] == 0).values.sum() * sp.expit(-logodds)
-      print(ind, gene, log_mean, log_disp, logodds)
       dist_data.data = bokeh.models.ColumnDataSource.from_df(pd.DataFrame({'x': .5 + grid, 'y': exp_count}))
     else:
       umi_data.data = bokeh.models.ColumnDataSource.from_df(pd.DataFrame(columns=['left', 'right', 'count']))
@@ -73,7 +83,7 @@ def init():
       con=conn))
 
 # These need to be separate because they have different dimension
-ind_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['ind', 'genotype', 'mean', 'bulk']))
+ind_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['ind', 'genotype', 'mean', 'disp', 'logodds', 'bulk']))
 ind_data.on_change('selected', update_umi)
 
 gene_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['gene', 'name', 'id', 'p_bulk', 'beta_bulk', 'p_sc', 'beta_sc']))
@@ -101,11 +111,21 @@ bulk_mean_by_geno.yaxis.axis_label = 'Bulk log CPM'
 
 umi = bokeh.plotting.figure(width=400, height=400, tools=[])
 umi.quad(source=umi_data, bottom=0, top='count', left='left', right='right', color='black')
-umi.line(source=dist_data, x='x', y='y', color='red')
+umi.line(source=dist_data, x='x', y='y', color='red', line_width=2)
 umi.xaxis.axis_label = 'Observed UMI'
 umi.yaxis.axis_label = 'Number of cells'
 
-layout = bokeh.layouts.layout([[qtls], [bulk_mean_by_geno, sc_mean_by_geno, umi]], sizing_mode='fixed')
+sc_disp_by_geno = bokeh.plotting.figure(width=400, height=400, tools=['tap'])
+sc_disp_by_geno.scatter(source=ind_data, x='genotype', y='disp', color='black', size=8)
+sc_disp_by_geno.xaxis.axis_label = 'Centered genotype'
+sc_disp_by_geno.yaxis.axis_label = 'Estimated log dispersion'
+
+sc_logodds_by_geno = bokeh.plotting.figure(width=400, height=400, tools=['tap'])
+sc_logodds_by_geno.scatter(source=ind_data, x='genotype', y='logodds', color='black', size=8)
+sc_logodds_by_geno.xaxis.axis_label = 'Centered genotype'
+sc_logodds_by_geno.yaxis.axis_label = 'Estimated dropout log odds'
+
+layout = bokeh.layouts.layout([[qtls], [bulk_mean_by_geno, sc_mean_by_geno, umi], [sc_disp_by_geno, sc_logodds_by_geno]], sizing_mode='fixed')
 
 doc = bokeh.io.curdoc()
 doc.title = 'scQTL browser'
