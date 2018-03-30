d1229cbe6c23d283f0fd5c32fa06c9c8a5ec5bf5
Abhishek Sarkar <aksarkar@alum.mit.edu>
2018-03-30 11:45:25 -0500

    Deploy the browser

--- browser/fano-qtls/main.py
+++ browser/fano-qtls/main.py
@@ -26,15 +26,14 @@ def update_gene(attr, old, new):
     gene = next(conn.execute('select gene from fano_qtls where fano_qtls.gene == ?;', (gene_data.data['gene'][selected[0]],)))[0]
     print('Selected {}'.format(gene))
     ind_data.data = bokeh.models.ColumnDataSource.from_df(pd.read_sql(
-      sql="""select fano_qtl_geno.ind, fano_qtl_geno.value as genotype, 
-      case when llr < 1 then nb_log_mean else zinb2_log_mean end as mean,
-      case when llr < 1 then nb_log_disp else zinb2_log_disp end as disp,
-      case when llr < 1 then -100 else zinb2_logodds end as logodds,
-      var.value as var from fano_qtl_geno, var, params where fano_qtl_geno.gene == ?
-      and fano_qtl_geno.gene == var.gene and var.gene == params.gene and
-      fano_qtl_geno.ind == var.ind and var.ind == params.ind;""",
+      sql="""select fano_qtl_geno.ind, fano_qtl_geno.value as genotype, log_mu,
+      log_phi, logodds, mean, var, var / mean as fano from
+      fano_qtl_geno, params where fano_qtl_geno.gene == ? and
+      fano_qtl_geno.gene == params.gene and fano_qtl_geno.ind == params.ind;""",
       params=(gene,),
       con=conn))
+    umi_data.data = bokeh.models.ColumnDataSource.from_df(pd.DataFrame(columns=['left', 'right', 'count']))
+    dist_data.data = bokeh.models.ColumnDataSource.from_df(pd.DataFrame(columns=['x', 'y']))
 
 def update_umi(attr, old, new):
   selected = ind_data.selected['1d']['indices']
@@ -53,16 +52,16 @@ def update_umi(attr, old, new):
       counts, _ = np.histogram(umi['value'].values, bins=edges)
       umi_data.data = bokeh.models.ColumnDataSource.from_df(pd.DataFrame({'left': edges[:-1], 'right': edges[1:], 'count': counts}))
 
-      params = pd.read_sql('select case when llr < 1 then nb_log_mean else zinb2_log_mean end as log_mean, case when llr < 1 then nb_log_disp else zinb2_log_disp end as log_disp, case when llr < 1 then null else zinb2_logodds end as logodds from params where gene == ? and ind == ?', con=conn, params=(gene, ind))
-      n = np.exp(params['log_disp'])
-      p = 1 / (1 + np.outer(umi['size'], np.exp(params['log_mean'] - params['log_disp'])))
+      params = pd.read_sql('select log_mu, log_phi, logodds from params where gene == ? and ind == ?', con=conn, params=(gene, ind))
+      n = np.exp(-params['log_phi'])
+      p = 1 / (1 + np.outer(umi['size'], np.exp(params['log_mu'] + params['log_phi'])))
       assert (n > 0).all(), 'n must be non-negative'
       assert (p >= 0).all(), 'p must be non-negative'
       assert (p <= 1).all(), 'p must be <= 1'
       G = st.nbinom(n=n.values.ravel(), p=p.ravel()).pmf
       grid = np.arange(19)
       pmf = np.array([G(x).mean() for x in grid])
-      if params.iloc[0]['logodds'] is not None:
+      if np.isfinite(params.iloc[0]['logodds']):
         pmf *= sp.expit(-params['logodds']).values
         pmf[0] += sp.expit(params['logodds']).values
       exp_count = umi.shape[0] * pmf
@@ -74,11 +73,13 @@ def update_umi(attr, old, new):
 def init():
   with sqlite3.connect(db) as conn:
     gene_data.data = bokeh.models.ColumnDataSource.from_df(pd.read_sql(
-      sql="""select gene, id, p_beta as p, beta from fano_qtls where fdr_pass order by p_beta;""",
+      sql="""select fano_qtls.gene, gene_info.name, id, p_beta as p, beta from fano_qtls,
+      gene_info where fano_qtls.gene == gene_info.gene and fdr_pass order by p_beta;""",
       con=conn))
 
 # These need to be separate because they have different dimension
-ind_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['ind', 'genotype', 'mean', 'disp', 'logodds', 'var']))
+ind_data = bokeh.models.ColumnDataSource(pd.DataFrame(
+  columns=['ind', 'genotype', 'log_mu', 'log_phi', 'logodds', 'mean', 'var', 'fano']))
 ind_data.on_change('selected', update_umi)
 
 gene_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['gene', 'id', 'p', 'beta']))
@@ -90,42 +91,56 @@ dist_data = bokeh.models.ColumnDataSource(pd.DataFrame(columns=['x', 'y']))
 # These need to be module scope because bokeh.server looks there
 qtls = bokeh.models.widgets.DataTable(
     source=gene_data,
-    columns=[bokeh.models.widgets.TableColumn(field=x, title=x) for x in ['gene', 'id', 'p', 'beta']],
+    columns=[bokeh.models.widgets.TableColumn(field=x, title=x) for x in ['gene', 'name', 'id', 'p', 'beta']],
     width=1200,
-    height=400)
+    height=300)
 
 hover = bokeh.models.HoverTool(tooltips=[('Individual', '@ind')])
 
-sc_var_by_geno = bokeh.plotting.figure(width=400, height=400, tools=['tap', hover])
-sc_var_by_geno.scatter(source=ind_data, x='genotype', y='var', color='black', size=8)
-sc_var_by_geno.xaxis.axis_label = 'Centered dosage'
-sc_var_by_geno.yaxis.axis_label = 'Single cell sample variance'
+sc_mu_by_geno = bokeh.plotting.figure(width=300, height=300, tools=['tap', hover])
+sc_mu_by_geno.scatter(source=ind_data, x='genotype', y='log_mu', color='black', size=8)
+sc_mu_by_geno.xaxis.axis_label = 'Dosage'
+sc_mu_by_geno.yaxis.axis_label = 'log(μ)'
 
-umi = bokeh.plotting.figure(width=400, height=400, tools=[])
+sc_phi_by_geno = bokeh.plotting.figure(width=300, height=300, tools=['tap', hover])
+sc_phi_by_geno.scatter(source=ind_data, x='genotype', y='log_phi', color='black', size=8)
+sc_phi_by_geno.xaxis.axis_label = 'Dosage'
+sc_phi_by_geno.yaxis.axis_label = 'log(φ)'
+
+sc_logodds_by_geno = bokeh.plotting.figure(width=300, height=300, tools=['tap', hover])
+sc_logodds_by_geno.scatter(source=ind_data, x='genotype', y='logodds', color='black', size=8)
+sc_logodds_by_geno.xaxis.axis_label = 'Dosage'
+sc_logodds_by_geno.yaxis.axis_label = 'logit(π)'
+
+sc_mean_by_geno = bokeh.plotting.figure(width=300, height=300, tools=['tap', hover])
+sc_mean_by_geno.scatter(source=ind_data, x='genotype', y='mean', color='black', size=8)
+sc_mean_by_geno.xaxis.axis_label = 'Dosage'
+sc_mean_by_geno.yaxis.axis_label = 'ZI-corrected mean'
+
+sc_var_by_geno = bokeh.plotting.figure(width=300, height=300, tools=['tap', hover])
+sc_var_by_geno.scatter(source=ind_data, x='genotype', y='var', color='black', size=8)
+sc_var_by_geno.xaxis.axis_label = 'Dosage'
+sc_var_by_geno.yaxis.axis_label = 'ZI-corrected variance'
+
+sc_fano_by_geno = bokeh.plotting.figure(width=300, height=300, tools=['tap', hover])
+sc_fano_by_geno.scatter(source=ind_data, x='genotype', y='fano', color='black', size=8)
+sc_fano_by_geno.xaxis.axis_label = 'Dosage'
+sc_fano_by_geno.yaxis.axis_label = 'ZI-corrected Fano factor'
+
+umi = bokeh.plotting.figure(width=300, height=300, tools=[])
 umi.quad(source=umi_data, bottom=0, top='count', left='left', right='right', color='black')
 umi.line(source=dist_data, x='x', y='y', color='red', line_width=2)
 umi.xaxis.axis_label = 'Observed UMI'
 umi.yaxis.axis_label = 'Number of cells'
 
-sc_mean_by_geno = bokeh.plotting.figure(width=400, height=400, tools=['tap', hover])
-sc_mean_by_geno.scatter(source=ind_data, x='genotype', y='mean', color='black', size=8)
-sc_mean_by_geno.xaxis.axis_label = 'Centered dosage'
-sc_mean_by_geno.yaxis.axis_label = 'Estimated single cell log mean expression'
-
-sc_disp_by_geno = bokeh.plotting.figure(width=400, height=400, tools=['tap', hover])
-sc_disp_by_geno.scatter(source=ind_data, x='genotype', y='disp', color='black', size=8)
-sc_disp_by_geno.xaxis.axis_label = 'Centered dosage'
-sc_disp_by_geno.yaxis.axis_label = 'Estimated single cell log disp expression'
-
-sc_logodds_by_geno = bokeh.plotting.figure(width=400, height=400, tools=['tap', hover])
-sc_logodds_by_geno.scatter(source=ind_data, x='genotype', y='logodds', color='black', size=8)
-sc_logodds_by_geno.xaxis.axis_label = 'Centered dosage'
-sc_logodds_by_geno.yaxis.axis_label = 'Estimated dropout log odds'
-
-layout = bokeh.layouts.layout([[qtls], [sc_var_by_geno, sc_mean_by_geno, sc_disp_by_geno], [umi, sc_logodds_by_geno]], sizing_mode='fixed')
+layout = bokeh.layouts.layout(
+  [[qtls],
+   [sc_mean_by_geno, sc_var_by_geno, sc_fano_by_geno],
+   [sc_mu_by_geno, sc_phi_by_geno, sc_logodds_by_geno, umi]],
+  sizing_mode='fixed')
 
 doc = bokeh.io.curdoc()
-doc.title = 'Variance QTL browser'
+doc.title = 'Fano QTL browser'
 doc.add_root(layout)
 
 init()
